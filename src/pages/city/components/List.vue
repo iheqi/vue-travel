50c6d7eb77466bd4cc2878be4a5f2be4b6b9ab8c
rubbish-man <993284758@qq.com>
2018-04-26 19:39:58 +0800

    ajax city page

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -13,53 +13,23 @@
         <div class="area">
           <div class="title border-topbottom">热门城市</div>
           <div class="button-list">
-            <div class="button-wrapper">
-              <div class="button">北京</div>
+            <div class="button-wrapper" v-for="item of hot" :key="item.id">
+              <div class="button">{{item.name}}</div>
             </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
-            <div class="button-wrapper">
-              <div class="button">北京</div>
-            </div>
+            
           </div>
         </div>
-        <div class="area">
-          <div class="title border-topbottom">A</div>
+        
+     
+        <div class="area" v-for='(item, key) of cities' :key='key'>
+          <div class="title border-topbottom">{{key}}</div>
           <div class="item-list">
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
+            <div class="item border-bottom" v-for='innerItem of item' :key="innerItem.id">
+              {{innerItem.name}}
+            </div>
           </div>
         </div>
-        <div class="area">
-          <div class="title border-topbottom">A</div>
-          <div class="item-list">
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-          </div>
-        </div>
-        <div class="area">
-          <div class="title border-topbottom">A</div>
-          <div class="item-list">
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-            <div class="item border-bottom">阿尔发</div>
-          </div>
-        </div>
+
       </div>
     </div>
 </template>
@@ -68,6 +38,10 @@ import Bscroll from 'better-scroll'
 
 export default {
   name: 'CityList',
+  props: {
+    cities: Object,
+    hot: Array
+  },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
   }
@@ -86,7 +60,7 @@ export default {
       border-color : #999
   .list
     overflow : hidden     /* 为什么会超出？又没固定高度 */
-    position : absolute   /* 固定list不让它滚动，为什么不滚动? */
+    position : absolute   /* 固定list不让它随滚轮滚动 */
     top : 1.58rem
     right : 0
     left : 0
