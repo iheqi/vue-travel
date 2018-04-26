f39cb848f5a95059cde67051fb16ed866a1644b9
rubbish-man <993284758@qq.com>
2018-04-26 13:52:34 +0800

    add city router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="header">
+      城市选择
+      <router-link to="/">
+        <div class="iconfont city-back">&#xe624;</div>
+      </router-link>
+      
+    </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader',
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl';
+  .header
+    overflow : hidden
+    height : $headerHeight
+    line-height : $headerHeight
+    text-align : center 
+    color : #fff
+    background-color : $bgColor
+    font-size: .32rem
+
+    position : relative
+    .city-back
+      width: .64rem
+      text-align: center 
+      font-size : .4rem
+      position : absolute
+      top : 0
+      left : 0
+      color : #fff
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
+import City from '@/pages/city/city'
+
 /* @表示src文件夹 */
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Home',
+    component: Home
+  }, {
+    path: '/city',
+    name: 'City',
+    component: City
+  }]
 })
